bf06362ddd8379fe2379d80883c6a662cfcbcdee
Ars-19 <ullaharsalan10@gmail.com>
2020-06-20 17:26:58 +0500

    hogaya bhai

---
+++ app.js
@@ -0,0 +1,232 @@
+// chapter 1 start
+
+// task 1
+
+// alert("thanks for your coming")
+
+// task 1
+
+
+// task 2
+
+// alert("Error ! Please enter a valid password.")
+
+// task 2
+
+
+
+// task 3
+
+// alert("Welcome to JS land..." + "\n" + " Happy Coding !")
+
+// task 3
+
+// task 4
+
+
+// alert("Welcome to JS land.")
+// alert("happy Coding" + "\n" + "Prevent this page from creating additional dialogs.")
+
+// task 4
+
+
+// chapter 1 end
+
+
+
+
+
+
+// chapter 2 start
+
+
+// task 1
+
+// var u="Username";
+// var myname="ARSALAN ULLAH KHAN";
+// document.write(myname);
+
+// task 1
+
+
+
+// task 2
+
+// var message=" HELLO WORLD";
+// alert( message)
+
+// task 2
+
+
+
+// task 3
+
+// var stn="Arsalan khan";
+// var sta="20years ";
+// var ste="Certified mobile Application development";
+// alert(stn)
+// alert(sta)
+// alert(ste)
+
+
+// task 3
+
+
+// task 4
+
+
+// var p="PIZZA" +"\n"+ "PIZZ" +"\n"+ "PIZ" +"\n"+ "PI" +"\n"+ "P";
+// alert(p)
+
+// task 4
+
+
+// task 6
+
+
+// var book="I am trying to learn from the book.A smarter way to learn JavaScript"
+// alert(book)
+
+// task 6
+
+
+
+// task 7
+
+// document.write( "\n" + "Yah! I can write HTML content through JavaScript")
+
+
+// task 7
+
+
+// chapter 2 end
+
+
+
+
+
+
+
+// chapter 3 start
+
+
+// task 1
+
+// var age="I am 20 years old";
+// alert(age)
+
+// task 1
+
+// task 2
+
+// var visit="You have visited this site 20 times";
+// alert(visit)
+
+// task 2
+
+
+
+// task 3
+
+// var Birthyear="My Birth Year is 2000" + "\n" + "Data type of my declared variable is number";
+// document.write(Birthyear)
+
+// task 3
+
+// task 4
+
+// var visitor="John Joe";
+// var product="T-shirts";
+// var quantity=5 ;
+// var company=" XYZ Clothing Store "
+// document.write(visitor + " ordered "+ quantity + product  + " on " + company )
+
+
+// task 4
+
+
+// chapter 3 end
+
+
+
+
+
+
+
+// chapter 4 start
+
+// task 1
+
+
+//  var a=3;
+//  var b=5;
+//  var c=a+b;
+//  document.write("sum of " + a + " and " + b + " is " + c)
+
+// document.write("<br>")
+
+//  var a=3;
+//  var b=5;
+//  var c=a-b;
+//  document.write("sbutraction of " + a + " and " + b + " is " + c)
+
+
+
+// document.write("<br>")
+//  var a=3;
+//   var b=5;
+// var c=a*b;
+//   document.write("multiplication of " + a + " and " + b + " is " + c)
+
+
+//   document.write("<br>")
+
+
+
+//   var a=3;
+//   var b=5;
+//    var c=a/b;
+//   document.write("division of " + a + " and " + b + " is " + c)
+// document.write("<br>")
+//  var a=3;
+//  var b=5;
+//   var c=b%a;
+// document.write("modulus of " + a + " and " + b + " is " + c)
+
+// document.write("<br>")
+//  var e=5;
+//  document.write("Initial Value is :" + e)
+//  document.write("<br>")
+//  var f=6;
+//  var g=f+7;
+//   document.write("value after increment is " + f++) 
+//   document.write("<br>")
+//   document.write( "value after addition is " + g)
+//   document.write("<br>")
+//   document.write( "value after decrement is " + --g)
+//   document.write("<br>")
+//  document.write( "value of remainder after division is " + --g%3)
+
+// task 3
+
+
+// task 4
+
+// var h=600;
+// var i=5*h;
+// document.write("Total cost to buy 5 ticketsto a movie is " + i)
+
+// task 4
+
+// task 5
+
+
+for(var i = 1; i = 10 ; i++){
+    document.write( "8" + " x "+ i + "=" + 8*i + "<br>")
+   }
+   
+// task 5
+
+// chapter 5 end
+
+
